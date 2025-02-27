c694533cfa64b641aee4ab347dd502e7b8dc1ca3
HitBlast <hitblastlive@gmail.com>
2025-02-27 16:44:27 +0600

    better init

--- igknite/core/chain.py
+++ igknite/core/chain.py
@@ -3,23 +3,19 @@ import logging
 from typing import List
 
 from decouple import config
-from disnake import Message
+from disnake.message import Message
 
 
 # Custom class for handling environment secrets and global variables..
 class KeyChain:
     def __init__(self) -> None:
-        self.discord_token = None
-        self.discord_owner_id = None
-        self.spotify_client_secret = None
-        self.spotify_client_id = None
         self.snipeables: List[Message] = []
 
         try:
-            self.discord_token = config('DISCORD_TOKEN', cast=str)
-            self.discord_owner_id = config('DISCORD_OWNER_ID', cast=int)
-            self.spotify_client_secret = config('SPOTIFY_CLIENT_SECRET', cast=str)
-            self.spotify_client_id = config('SPOTIFY_CLIENT_ID', cast=str)
+            self.discord_token = config('DISCORD_TOKEN', default=None, cast=str)
+            self.discord_owner_id = config('DISCORD_OWNER_ID', default=None, cast=int)
+            self.spotify_client_secret = config('SPOTIFY_CLIENT_SECRET', default=None, cast=str)
+            self.spotify_client_id = config('SPOTIFY_CLIENT_ID', default=None, cast=str)
 
         except ValueError:
             logging.error(
